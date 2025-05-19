9311e8d117ac1e71f8c007f821ae910d7c52ec7f
omkar-mohanty <franzohouser@gmail.com>
2025-05-19 11:42:50 +0530

    update syntax

--- config/modules/plugins/colorscheme.nix
+++ config/modules/plugins/colorscheme.nix
@@ -2,12 +2,14 @@
   colorschemes = {
     # modus.enable = true;
     # rose-pine.enable = true;
+    #xocarbon.enable = true;
     tokyonight = {
-        enable = true;
-        settings = {
-          # Like many other themes, this one has different styles, and you could load
-          # any other, such as 'storm', 'moon', or 'day'.
-          style = "night";
-        };
+      enable = true;
+      settings = {
+        # Like many other themes, this one has different styles, and you could load
+        # any other, such as 'storm', 'moon', or 'day'.
+        style = "night";
+      };
+    };
   };
 }
