a7f60b757744c54548a814549ef34202c92fd236
omkar-mohanty <franzohouser@gmail.com>
2025-05-20 17:01:31 +0530

    init

--- config/modules/plugins/colorscheme.nix
+++ config/modules/plugins/colorscheme.nix
@@ -2,14 +2,14 @@
   colorschemes = {
     # modus.enable = true;
     # rose-pine.enable = true;
-    #xocarbon.enable = true;
-    tokyonight = {
-      enable = true;
-      settings = {
-        # Like many other themes, this one has different styles, and you could load
-        # any other, such as 'storm', 'moon', or 'day'.
-        style = "night";
-      };
-    };
+    xocarbon.enable = true;
+    #tokyonight = {
+    #  enable = true;
+    #  settings = {
+    #    # Like many other themes, this one has different styles, and you could load
+    #    # any other, such as 'storm', 'moon', or 'day'.
+    #    style = "night";
+    #  };
+    #};
   };
 }
